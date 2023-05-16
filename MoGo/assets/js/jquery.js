b5cb50542f2624ad00e4eacc3790985eb6934de9
Peccopa <orbital.logistics@yandex.ru>
2023-05-16 08:59:09 +0300

    jquery1605

---
+++ MoGo/assets/js/jquery.js
@@ -0,0 +1,32 @@
+
+//jquery
+
+$(function() {
+
+    let header = $("#header"),
+    introH = $("#intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+
+    /* fixed header */
+    checkScroll(scrollOffset);
+
+
+    $(window).on("scroll", function(){
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+       if (scrollOffset >= introH) {
+        header.addClass("fixed");
+       } else {
+        header.removeClass("fixed");
+       }
+
+    }
+
+
+    /* smooth scroll */
+
+});
