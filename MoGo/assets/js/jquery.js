6bc932b6b12648510d9068f7dddbfb5f5603ed90
Peccopa <orbital.logistics@yandex.ru>
2023-05-17 10:20:53 +0300

    slider

--- MoGo/assets/js/jquery.js
+++ MoGo/assets/js/jquery.js
@@ -28,5 +28,46 @@ $(function() {
 
 
     /* smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+
+
+    });
+
+
+
+    /*menu nav toggle*/
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        
+        $("#nav").toggleClass("active");
+    });
+
+    /*collaps*/
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        // $this.toggleClass("active");
+        $(blockId).slideToggle();
+        $this.toggleClass("active");
+
+    });
+
+
 
 });
